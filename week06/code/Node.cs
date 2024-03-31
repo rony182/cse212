2fb5a61cf63bdd735e232c0de16de3867d4a9da5
Rony <rnicksonaxter@gmail.com>
2024-03-31 17:20:54 -0300

    adding code to personal assignment week 06

--- week06/code/Node.cs
+++ week06/code/Node.cs
@@ -1,21 +1,30 @@
-public class Node {
+public class Node
+{
     public int Data { get; set; }
     public Node? Right { get; private set; }
     public Node? Left { get; private set; }
 
-    public Node(int data) {
+    public Node(int data)
+    {
         this.Data = data;
     }
 
-    public void Insert(int value) {
-        if (value < Data) {
+    public void Insert(int value)
+    {
+        if (value == Data)
+        {
+            return;
+        }
+        else if (value < Data)
+        {
             // Insert to the left
             if (Left is null)
                 Left = new Node(value);
             else
                 Left.Insert(value);
         }
-        else {
+        else
+        {
             // Insert to the right
             if (Right is null)
                 Right = new Node(value);
@@ -24,13 +33,45 @@ public class Node {
         }
     }
 
-    public bool Contains(int value) {
-        // TODO Start Problem 2
-        return false;
+    public bool Contains(int value)
+    {
+        if (value == Data)
+        {
+            return true;
+        }
+        else if (value < Data)
+        {
+            // Search to the left
+            if (Left is null)
+                return false;
+            else
+                return Left.Contains(value);
+        }
+        else
+        {
+            // Search to the right
+            if (Right is null)
+                return false;
+            else
+                return Right.Contains(value);
+        }
     }
 
-    public int GetHeight() {
-        // TODO Start Problem 4
-        return 0; // Replace this line with the correct return statement(s)
+    public int GetHeight()
+    {
+        int GetHeight(Node? node)
+        {
+            if (node is null)
+            {
+                return 0;
+            }
+            else
+            {
+                int leftHeight = GetHeight(node.Left);
+                int rightHeight = GetHeight(node.Right);
+                return 1 + Math.Max(leftHeight, rightHeight);
+            }
+        }
+        return GetHeight(this);
     }
 }
